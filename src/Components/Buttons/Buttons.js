e0f556700375a7c695298b8af8eaccde2a76e2f5
hoanghaonguyen <haohao6345@gmail.com>
2024-07-23 08:51:35 +0700

    add buoi22

---
+++ src/Components/Buttons/Buttons.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Container, Row, Col } from "reactstrap";
+
+function Buttons(props) {
+  const { listCateTour, handleClick, city } = props;
+
+  return (
+    <div>
+      <Container style={{ width: "50%", margin: "20px auto" }}>
+        <Row md="4">
+          {listCateTour.map((tour, index) => (
+            <Col key={index}>
+              <Button
+                onClick={() => handleClick({ id: tour.id, title: tour.title })}
+                style={{
+                  padding: "5px 10px",
+                  height: "40px",
+                  backgroundColor:
+                    tour.id === city.id ? "var(--primary-color)" : "#fff",
+                  color: tour.id === city.id ? "#fff" : "#000",
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  border: "none",
+                  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                }}
+              >
+                {tour.title}
+              </Button>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Buttons;
